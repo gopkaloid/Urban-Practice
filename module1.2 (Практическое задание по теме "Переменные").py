c08ea4d9ff030bae10a78f9e94939d625768546b
Igor Gopkalo <gopkalo301@gmail.com>
2024-09-18 23:47:30 +1000

    Update module1.2 (Практическое задание по теме "Переменные").py

--- module1.2 (Практическое задание по теме "Переменные").py
+++ module1.2 (Практическое задание по теме "Переменные").py
@@ -1,5 +1,5 @@
 The_number_of_completed_HA = 12
 Number_of_hours_spent = 1.5
 Course_name = 'Python'
-Time_for_one_task = int (12 / 1.5)
+Time_for_one_task = int (The_number_of_completed_HA / Number_of_hours_spent)
 print ('Cours:', Course_name, ',', 'total tasks:', The_number_of_completed_HA, ',', 'hours spent:', Number_of_hours_spent, ',', 'average execution time', Time_for_one_task, 'hours')
