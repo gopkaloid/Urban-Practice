c54578f752c7b465a957b855d6cb5d7381171752
Igor Gopkalo <gopkalo301@gmail.com>
2024-09-18 23:21:15 +1000

    Add files via upload

---
+++ module1.2 (Практическое задание по теме "Переменные").py
@@ -0,0 +1,5 @@
+The_number_of_completed_HA = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = int (12 / 1.5)
+print ('Cours:', Course_name, ',', 'total tasks:', The_number_of_completed_HA, ',', 'hours spent:', Number_of_hours_spent, ',', 'average execution time', Time_for_one_task, 'hours')
