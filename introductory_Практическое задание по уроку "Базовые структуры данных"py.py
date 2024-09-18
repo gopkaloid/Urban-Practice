b734752ec971c800d23fc3a4b1cee41a5f4ba577
Igor Gopkalo <gopkalo301@gmail.com>
2024-09-18 23:26:11 +1000

    Add files via upload

---
+++ introductory_Практическое задание по уроку "Базовые структуры данных"py.py
@@ -0,0 +1,4 @@
+print((9 * 5) ** 0, 5 / 1)  #1st program
+print(9.99 > 9, 98 and 1000 != 1000.1)  #2st program
+print(2 * 2 + 2 == (2 + 2) * 2)  #3st program
+print(int (float('123.456') * 10) % 10)  #4st program
