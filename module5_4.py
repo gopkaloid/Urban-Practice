bfbfef6914f1f6a58a5aeac7c977f453f432cd96
Igor Gopkalo <gopkalo301@gmail.com>
2024-11-03 13:31:33 +1000

    Add files via upload

---
+++ module5_4.py
@@ -0,0 +1,35 @@
+class House:
+
+    houses_history = []
+
+    def __init__(self, name, floors):
+        self.name = name
+        self.floors = floors
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.floors})"
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+
+        if len(args) > 0:
+            cls.houses_history.append(args[0])
+        return instance
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 30)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
